21e6821810f3347e4821199693da1e5a26b34638
ahmed.awadallah <aawadall@ualberta.ca>
2017-09-08 16:39:45 -0400

    Fixing issues #51 and #52

--- PyProjManCore/proj_man.py
+++ PyProjManCore/proj_man.py
@@ -3,9 +3,27 @@ from PyProjManCore.task import Task
 
 class ProjMan:
     """Project Manager Object, uses Tasks and Time Elements"""
-    def __init__(self, root: Task):
+    def __init__(self, root: Task = None, name = None):
         """
         Initialize a project, with minimum one task as root task
         :type root: object
         """
-        _root = root
+        self._root = root
+        self._name = name
+
+    @property
+    def root(self):
+        """Root Task of this project"""
+        return self._root
+
+    @property
+    def name(self):
+        """Name of a project"""
+        return self._name
+
+    def add_child(self, child):
+        self._root.append_dependant(child)
+
+    def add_parent(self, parent):
+        self._root.append_prerequisite(parent)
+        self._root = parent
--- UnitTesting/test_proj_man.py
+++ UnitTesting/test_proj_man.py
@@ -1,15 +1,62 @@
 from unittest import TestCase
+from PyProjManCore.proj_man import ProjMan
+from PyProjManCore.task import Task
 
 
 class TestProjMan(TestCase):
     def test_init(self):
-        self.fail("Not Implemented")
+        """Check Type of ProjMan"""
+        pm = ProjMan()
+        self.assertTrue(isinstance(pm, ProjMan))
+
+    def test_root(self):
+        """Can create a new project with a root Task"""
+        root = Task("Root Task")
+        pm = ProjMan(root)
+        self.assertEqual(root, pm.root)
+
+    def test_name(self):
+        """Can we define a name in the constructor?"""
+        project_name = "Some Project Name"
+        pm = ProjMan(None,project_name)
+        self.assertEqual(project_name, pm.name)
 
     def test_add_child(self):
-        self.fail("Not implemented")
+        """PM Can add a child"""
+        root = Task("Root Task")
+        pm = ProjMan(root)
+        child = Task("Child Task")
+        pm.add_child(child)
+        self.assertIn(child, root.dependants)
+
+    def test_add_child2(self):
+        """PM Adding a child implies root is a parent for the child"""
+        root = Task("Root Task")
+        pm = ProjMan(root)
+        child = Task("Child Task")
+        pm.add_child(child)
+        self.assertIn(root, child.prerequisites)
 
     def test_add_parent(self):
-        self.fail("Not implemented")
+        """Can Add Parent to Root Task"""
+        root = Task("Root Task")
+        pm = ProjMan(root)
+        parent = Task("Parent Task")
+        pm.add_parent(parent)
+        self.assertIn(parent, root.prerequisites)
 
-    def test_project_stats(self):
-        self.fail("Not implemented")
+    def test_add_parent_rebase1(self):
+        """Adding Parent to root Task, Rebase Root to Parent"""
+        root = Task("Root Task")
+        pm = ProjMan(root)
+        parent = Task("Parent Task")
+        pm.add_parent(parent)
+        self.assertEqual(parent, pm.root)
+
+    def test_add_parent_rebase2(self):
+        """Adding Parent to root Task, Makes former root a child to parent"""
+        root = Task("Root Task")
+        pm = ProjMan(root)
+        parent = Task("Parent Task")
+        pm.add_parent(parent)
+        self.assertIn(root, pm.root.dependants)
