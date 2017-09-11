ca3ec9ac5bad1244a5d90b5cc5cd27258d9e8573
ahmed.awadallah <aawadall@ualberta.ca>
2017-09-11 13:51:04 -0400

    Removed Upstream down stream from Task class, this should be handled by ProjMan class

--- UnitTesting/test_task_happy.py
+++ UnitTesting/test_task_happy.py
@@ -15,14 +15,6 @@ class TestTaskOperationsHappyPath(TestCase):
         target.prerequisites.append(prereq)
         self.assertIn(prereq, target.prerequisites)
 
-    def test_parent_child_upstream(self):
-        """Test that defining a prerequisite implies defining the caller as a dependant"""
-        self.fail("Not implemented")
-
-    def test_parent_child_downstream(self):
-        """Test that defining a dependant implies defining the caller as a prerequisite"""
-        self.fail("Not implemented")
-
     def test_append_dependants(self):
         """
         Create a Task
@@ -33,14 +25,3 @@ class TestTaskOperationsHappyPath(TestCase):
         dep = Task()
         target.append_dependant(dep)
         self.assertIn(dep, target.dependants)
-
-    def test_crawl_up(self):
-        """Test Crawl Up a Task"""
-        # Define a Task
-        # Define 3 Layers of tasks
-        # Assign each layer the layer over it as a prerequisite
-        self.fail("Not implemented")
-
-    def test_crawl_down(self):
-        """Test Crawl Down a Task"""
-        self.fail("Not implemented")
