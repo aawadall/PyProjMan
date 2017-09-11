7c571e86532e028ae122decd4cf67f613a9cad09
ahmed.awadallah <aawadall@ualberta.ca>
2017-09-11 15:30:12 -0400

    First green wave of parser

--- UnitTesting/test_parser.py
+++ UnitTesting/test_parser.py
@@ -11,6 +11,7 @@ def helper_load_cfg_file():
     p.load_parser_data(os.path.join(os.getcwd(),  'test.json'))
     return p
 
+
 class TestPyProjManParser(TestCase):
     """Test PyProjManParser"""
     def test_init(self):
@@ -22,42 +23,69 @@ class TestPyProjManParser(TestCase):
     def test_load_parser_data_version(self):
         """Can read Version from Config file"""
         p = helper_load_cfg_file()
-        self.assertEqual('0.0.2',p._version)
+        target = '0.0.2'
+        self.assertEqual(target,p._version)
 
     def test_load_parser_data_release(self):
         """Can read Release from Config file"""
         p = helper_load_cfg_file()
-        self.assertEqual('alpha',p._release)
+        target = 'alpha'
+        self.assertEqual(target,p._release)
 
     def test_load_parser_data_ignore_case(self):
-        self.fail()
+        """Can read Ignore Case from Config file"""
+        p = helper_load_cfg_file()
+        target = True
+        self.assertEqual(target, p._ignore_case)
 
     def test_load_parser_data_primitives(self):
-        self.fail()
+        """Can read Primitives from Config file"""
+        p = helper_load_cfg_file()
+        target = {"ADD": 256}
+        key = 'ADD'
+        value = 256
+        target = {key: value}
+        self.assertEqual(target[key], p._primatives[key])
+
 
     def test_load_parser_data_verbs(self):
-        self.fail()
+        """Can read Verbs from Config file"""
+        p = helper_load_cfg_file()
+        target = {"add": "ADD"}
+        key = 'add'
+        value = 'ADD'
+        target = {key: value}
+        self.assertEqual(target[key], p._verbs[key])
 
     def test_load_parser_data_parameters(self):
-        self.fail()
+        """Can read Parameters from Config file"""
+        p = helper_load_cfg_file()
+        key = 'best'
+        value = 'BEST'
+        target = {key: value}
+        self.assertEqual(target[key], p._parameters[key])
+
 
     def test_load_parser_data_decorators(self):
-        self.fail()
+        """Can read Decorators from Config file"""
+        p = helper_load_cfg_file()
+        key = 'on'
+        value = 'ON'
+        target = {key: value}
+        self.assertEqual(target[key], p._decorators[key])
 
     def test_load_parser_data_reply(self):
-        self.fail()
+        """Can read Reply from Config file"""
+        p = helper_load_cfg_file()
+        key = 'SUCCESS'
+        value = 'Success'
+        target = {key: value}
+        self.assertEqual(target[key], p._reply[key])
 
     def test_load_parser_data_err_codes(self):
-        self.fail()
-
-    def test_listen(self):
-        self.fail()
-
-    def test_parse(self):
-        self.fail()
-
-    def test_feed_back(self):
-        self.fail()
-
-    def test_hook(self):
-        self.fail()
+        """Can read Error Codes from Config file"""
+        p = helper_load_cfg_file()
+        key = 100
+        value = 'Success'
+        target = {key: value}
+        self.assertEqual(target[key], p._error_codes[key])
